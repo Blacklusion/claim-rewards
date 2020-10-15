da11db3451ae8919affec5c70d3534f0e647bfcb
Leonard Scheidemantel <leonard.scheidemantel@googlemail.com>
2020-10-15 16:05:54 +0200

    Finalize code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Claim Rewards
+
 Claim rewards on EOSIO Blockchains once per day (or more). Written in Typescript and deployed with Docker.
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,163 @@
 import { logger } from "./common";
+import { Api, JsonRpc, RpcError } from "eosjs";
+import { JsSignatureProvider } from "eosjs/dist/eosjs-jssig";
+import * as cron from "cron";
+import fetch from "node-fetch";
+import * as config from "config";
+
+let cronPattern;
+let chains = [];
+let chainSettings;
+
+
+main();
+/**
+ * Read Config and trigger cron based execution
+ * @return if not all settings were specified
+ */
+function main() {
+  let allVariablesSet = true;
+
+  // Read Cron Pattern from local.toml
+  try {
+    cronPattern = config.get("general.cronPattern");
+  } catch (e) {
+    logger.error("No cron pattern was provided");
+    allVariablesSet = false;
+  }
+
+  // Read activated chains from local.toml
+  try {
+    chains = config.get("general.chains");
+
+    if (chains.length == 0) {
+      logger.error("general.chains setting was provided. But it did not contain any entries!");
+      allVariablesSet = false;
+    }
+  } catch (e) {
+    logger.error("No general.chains was provided");
+    allVariablesSet = false;
+  }
+
+  // Read chain Setings from local.toml
+  try {
+    chainSettings = config.get("chains");
+  } catch (e) {
+    logger.error("No chains sections was provided");
+    allVariablesSet = false;
+  }
+
+  // Test if for every specified chain all necessary settings are set
+  for (let chainId of chains) {
+    const chain = chainSettings[chainId];
+    try {
+      if (!chain["apiUrl"]) {
+        logger.error("No apiUrl was provided for " + chainId);
+        allVariablesSet = false;
+      }
+
+      if (!chain["producerName"]) {
+        logger.error("No producerName was provided for " + chainId);
+        allVariablesSet = false;
+      }
+
+      if (!chain["permissionName"]) {
+        logger.error('No permissionName (e.g. "claimrewards" or "active") was provided for ' + chainId);
+        allVariablesSet = false;
+      }
+
+      if (!chain["privateKey"]) {
+        logger.error("No privateKey was provided for " + chainId);
+        allVariablesSet = false;
+      }
+    } catch (e) {
+      logger.error("Could not read settings for " + chainId + ":", e);
+      allVariablesSet = false;
+    }
+  }
+
+  // Stop execution if some settings are missing
+  if (!allVariablesSet) {
+    logger.fatal("Not all variables were set. Aborting execution!");
+    return 1;
+  }
+
+  /**
+   * Claim rewards
+   */
+
+  const job = new cron.CronJob({
+    cronTime: cronPattern,
+    onTick: () => {
+      console.log("\n")
+      for (const chainId of chains) {
+        claimRewards(chainSettings, chainId);
+      }
+    },
+  });
+
+  job.start();
+
+  logger.info("++++++++++++++++++++++++++++++ STARTUP COMPLETE ++++++++++++++++++++++++++++++");
+  logger.info('Claiming Rewards at "' + cronPattern + '" for ' + chains.toString().replace(",", ", "));
+  logger.info("Current Date: " + new Date());
+  logger.info("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+}
+
+/**
+ * Claims rewards for the specified chain ONCE
+ * @param chains = all settings for all chains as specified under the chains section in local.toml
+ * @param chainId = name of the chain for which the rewards shall be claimed (must match name in local.toml)
+ */
+async function claimRewards(chains: any, chainId: any) {
+  // Eos js settings
+  const chain = chains[chainId];
+  const rpc = new JsonRpc(chain["apiUrl"], { fetch });
+  const api = new Api({
+    rpc: rpc,
+    signatureProvider: new JsSignatureProvider([chain["privateKey"]]),
+    textDecoder: new TextDecoder(),
+    textEncoder: new TextEncoder(),
+  });
+
+  /**
+   * Perform Claim
+   */
+  try {
+    const response = await api.transact(
+      {
+        actions: [
+          {
+            account: "eosio",
+            name: "claimrewards",
+            authorization: [
+              {
+                actor: chain["producerName"],
+                permission: chain["permissionName"],
+              },
+            ],
+            data: {
+              owner: chain["producerName"],
+            },
+          },
+        ],
+      },
+      {
+        broadcast: true,
+        blocksBehind: 3,
+        expireSeconds: 60,
+        sign: true,
+      }
+    );
+
+    // Claim successful
+    logger.info(chainId + " => Successfully claimed rewards!");
+    logger.debug(response);
+  } catch (e) {
+    // Claim failed
+    logger.error(chainId + " => Failed to claim rewards: " + e.message);
+
+    // Set logger to debug in common.ts to see the message (default = info)
+    if (e instanceof RpcError) logger.debug(JSON.stringify(e.json, null, 2));
+  }
+}
